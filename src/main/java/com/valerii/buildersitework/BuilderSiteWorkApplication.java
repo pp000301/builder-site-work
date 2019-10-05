eebcb3fb52d73850f072bea4857b3fc7b6723280
pp000301 <pp000302@gmail.com>
2019-10-05 21:13:09 +0300

    Initial commit

---
+++ src/main/java/com/valerii/buildersitework/BuilderSiteWorkApplication.java
@@ -0,0 +1,13 @@
+package com.valerii.buildersitework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuilderSiteWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuilderSiteWorkApplication.class, args);
+    }
+
+}
